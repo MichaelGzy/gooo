770802472862a175367e67ec36e60b8cc6c9cb20
michaelgzy <429534533@qq.com>
2021-01-15 15:57:31 +0800

    ref

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/MichaelGzy/gooo/oss"
+)
+
+func init() {
+	fmt.Println("welcome to gooo")
+}
 
 func main() {
 	fmt.Println("hello world")
+	oss.SayHi()
 }
--- oss/gooo.go
+++ oss/gooo.go
@@ -1,7 +1,11 @@
 package oss
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/MichaelGzy/gooo/srv"
+)
 
 func SayHi() {
 	fmt.Println("Hi_oss")
+	srv.SayHi()
 }
